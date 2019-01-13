fb7748257a88acbc65a6feb9fe032250fe517492
unsocialism <timoniori@gmail.com>
2019-01-13 12:43:53 +0100

    Add files via upload

---
+++ Tutorial_14_logging_audit_trails/Tutorial_14_logging_audit_trails/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Tutorial_14_logging_audit_trails
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
